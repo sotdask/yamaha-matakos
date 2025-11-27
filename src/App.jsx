e873e3130b29c92e8d918b4bda59329ecb0adf5b
sotdask <sotdaskalou@gmail.com>
2025-11-27 16:08:26 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Handle GitHub Pages 404 redirect
+    if (location.search) {
+      const params = new URLSearchParams(location.search);
+      const redirectPath = params.get('/');
+      if (redirectPath) {
+        const path = redirectPath.replace(/~and~/g, '&');
+        navigate(path, { replace: true });
+      }
+    }
+  }, [location, navigate]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
